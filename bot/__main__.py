e4231135d5d5c540c5915ed132c9ea239f8e7306
Aaron-p-saji <65056958+Aaron-p-saji@users.noreply.github.com>
2022-07-08 21:38:44 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -4,9 +4,9 @@ from subprocess import run as srun, check_output
 from psutil import disk_usage, cpu_percent, swap_memory, cpu_count, virtual_memory, net_io_counters, boot_time
 from time import time
 from sys import executable
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, helpers
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.constants import ParseMode
-from telegram.ext import Application, CallbackQueryHandler, CommandHandler, ContextTypes, filters
+from telegram.ext import Application, CallbackQueryHandler, CommandHandler, ContextTypes, filters, helpers
 import logging
 
 from bot import bot, dispatcher, updater, botStartTime, IGNORE_PENDING_REQUESTS, LOGGER, Interval, INCOMPLETE_TASK_NOTIFIER, DB_URI, alive, app, main_loop
